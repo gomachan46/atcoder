4379155f5e483f77ca587ed0f8eac3e4d72536b1
gomachan <go_nakanishi@voyagegroup.com>
2018-10-05 14:16:31 +0900

    別解的な？

--- welcome_to_atcoder/hoge.rb
+++ welcome_to_atcoder/hoge.rb
@@ -1,8 +1,5 @@
-# 整数の入力
-a = gets.to_i
-# スペース区切りの整数の入力
-b,c=gets.chomp.split(" ").map(&:to_i);
-# 文字列の入力
-s = gets.chomp
-# 出力
-print("#{a+b+c} #{s}\n")
+inputs = readlines
+a = inputs[0].to_i
+b , c = inputs[1].chomp.split(" ").map(&:to_i)
+s = inputs[2].chomp
+puts("#{a + b + c} #{s}")
