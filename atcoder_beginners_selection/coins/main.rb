82ed4d9be6ce99681987259155e972c7f5a11632
gomachan46 <shiro.gomachan46@gmail.com>
2018-10-12 17:58:54 +0900

    ちょっとループ節約

--- atcoder_beginners_selection/coins/main.rb
+++ atcoder_beginners_selection/coins/main.rb
@@ -3,6 +3,13 @@ b = gets.to_i
 c = gets.to_i
 x = gets.to_i
 
+a_max = (x / 500)
+b_max = (x / 100)
+c_max = (x / 50)
+a = a_max if a > a_max
+b = b_max if b > b_max
+c = c_max if c > c_max
+
 result = 0
 (0..a).each do |aa|
   (0..b).each do |bb|
