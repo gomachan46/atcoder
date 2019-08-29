bad1c0b6c3e0032d82f736cfe1762deb30f03c0b
gomachan46 <shiro.gomachan46@gmail.com>
2019-08-29 18:57:42 +0900

    C解けた

--- abc137/c.rb
+++ abc137/c.rb
@@ -1,3 +1,17 @@
+class Integer
+  def combination(k)
+    return 1 if k.zero?
+
+    (self - k + 1..self).inject(:*) / k.factorial
+  end
+
+  def factorial
+    return 1 if self.zero?
+
+    (1..self).inject(:*)
+  end
+end
+
 n = gets.chomp.to_i
-s = (1..n).map { gets.chomp.chars.sort }
-p s.group_by { |sn| sn }.select { |_, v| v.count > 1 }.values.inject(0) { |sum, v| sum + v.combination(2).count }
+s = (1..n).map { gets.chomp.chars.sort.join.intern }
+puts s.group_by { |sn| sn }.values.inject(0) { |sum, v| sum + v.count.combination(2) }
