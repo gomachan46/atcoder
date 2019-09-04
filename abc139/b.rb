01214f9918257ada817a9f0999328b6c5dc2c448
gomachan46 <shiro.gomachan46@gmail.com>
2019-09-04 14:12:59 +0900

    ちょっときれいに

--- abc139/b.rb
+++ abc139/b.rb
@@ -1,15 +1,3 @@
 a, b = gets.chomp.split(' ').map(&:to_i)
 
-current = 1
-count = 0
-
-def add_plug(a)
-  a - 1
-end
-
-while b > current
-  current += add_plug(a)
-  count += 1
-end
-
-puts count
+puts (b - 1).quo(a - 1).ceil
--- abc139/c.rb
+++ abc139/c.rb
@@ -1,14 +1,11 @@
 n = gets.chomp.to_i
 h = gets.chomp.split(' ').map(&:to_i)
-h << 10 ** 9 + 1
 
 combo = Array.new(n, 0)
-combo.each_with_index do |_, i|
+(n - 1).times do |i|
   if h[i] >= h[i + 1]
     combo[i] = 1
-    if i > 0
-      combo[i] += combo[i - 1]
-    end
+    combo[i] += combo[i - 1] if i > 0
   end
 end
 
